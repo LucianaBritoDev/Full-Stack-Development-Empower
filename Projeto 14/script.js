0175d5b41386ad0d99ec6fa3928e6e99151241ec
Luciana Brito <lucianabritodev@gmail.com>
2024-10-21 11:36:07 -0300

    commit initial

---
+++ Projeto 14/script.js
@@ -0,0 +1,9 @@
+console.log("Olá, mundo!");
+
+console.log("Meu nome é Luciana!");
+
+console.log("Este é meu arquivo inicial em JavaScript.");
+
+console.log(
+  "Estou muito feliz em poder utilizar essa linguagem de programação!"
+);
