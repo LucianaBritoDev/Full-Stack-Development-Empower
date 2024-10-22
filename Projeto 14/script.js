559891ac0bca197b14f1299ce7d8848f1ca6ca87
Luciana Brito <lucianabritodev@gmail.com>
2024-10-22 11:24:13 -0300

    Delete Projeto 14 directory

--- Projeto 14/script.js
+++
@@ -1,9 +0,0 @@
-console.log("Olá, mundo!");
-
-console.log("Meu nome é Luciana!");
-
-console.log("Este é meu arquivo inicial em JavaScript.");
-
-console.log(
-  "Estou muito feliz em poder utilizar essa linguagem de programação!"
-);
