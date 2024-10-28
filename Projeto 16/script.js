9899c1a0fe4eedcf887e6ebc76a9830d88afd539
Luciana Brito <lucianabritodev@gmail.com>
2024-10-28 16:25:39 -0300

    commit initial

---
+++ Projeto 16/script.js
@@ -0,0 +1,90 @@
+//console.log("Aula de Loops");
+
+// Loops são laços de repetição. Uma forma de o computador executar algo repetidas vezes.
+
+//for: quando se sabe precisar quantas vezes será necessário utilizar.
+
+// for (inicialização; condição; incremento) {
+// bloco que será executado
+//}
+
+//for (let i = 1; i <= 10; i++) {
+//i = i + 1 (acréscimo de 1 em 1)
+//console.log(i);
+//}
+
+//for (let j = 2; j <= 12; j += 2) {
+//console.log(j);
+//} // (acréscimo de 2 em 2 ou mais)
+
+//while: quando não se sabe precisar quantas vezes será necessário utilizar.
+
+// let nome = valor
+//while(condição) {
+//código a ser executado
+//}
+
+//let k = 20;
+//while (k >= 0) {
+//console.log(k);
+//k--;
+//}
+
+//for (let f = 25; f > 0; f--) {
+//console.log(f);
+//}
+
+//for (let crescente = 1; crescente
+//<= 25; crescente++) {
+//console.log(crescente);
+//}
+
+// And e Or
+// And &&
+
+//let login = "Steve Rogers";
+
+//let senha = 1234;
+
+//if (login == "Steve Rogers" && senha == 1234) {
+//console.log("Acesso liberado!");
+//} else {
+//console.log("Acesso negado!");
+//}
+
+// Or ||
+
+//let membrosDoClube = false;
+//let temConvite = false;
+
+//if (membrosDoClube || temConvite) {
+//console.log("Seja bem-vindo(a) à festa!");
+//} else {
+//console.log(
+//"Você não tem convite e nem é membro do membrosDoClube, logo não pode entrar. Pague 10 reais e vire membro do nosso clube"
+//);
+//}
+
+//let frequenciaAluno = 95;
+//let softSkills = true;
+//let entregaDesafios = true;
+
+//if (frequenciaAluno >= 75 && softSkills && entregaDesafios) {
+//console.log("Você receberá o seu certificado de Dev FullStack!");
+//} else {
+//console.log("Sinto muito! Reprovado!");
+//}
+
+//let PlanejarViagemParaLondres = 2026;
+//let ConseguirUmTrabalho = true;
+//let OrganizarViagem = true;
+
+//if (
+//PlanejarViagemParaLondres >= 2026 &&
+//ConseguirUmTrabalho &&
+//OrganizarViagem
+//) {
+//console.log("Você irá viajar em 2026!");
+//} else {
+//console.log("Você terá que esperar mais um ano!");
+//}
